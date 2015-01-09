e099b86360df54801e697f3af0ed8e38b04ea43b
Nikhil Unni <nunni2@illinois.edu>
2015-01-09 12:35:56 -0800

    marginal improvement

--- event.js
+++ event.js
@@ -1,5 +1,18 @@
+var dictionary = {};
+var prefix =  'docs.google.com/viewer?url=';
+
+
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
-    return {redirectUrl: 'http://docs.google.com/viewer?url='+details.url+'&embedded=true'};
+    var oldUrl = null;
+    chrome.tabs.get(details.tabId,
+                    function(tab) {
+                        oldUrl = tab.url;
+                        chrome.extension.getBackgroundPage().console.log(oldUrl);                        
+                    });
+    chrome.extension.getBackgroundPage().console.log("NOT TAB");
+    return {redirectUrl: 'http://docs.google.com/viewer?url='+details.url+'&embedded=false'};
+
+    
 }, {
     urls: ['*://*/*.docx', '*://*/*.doc', '*://*/*.pptx', '*://*/*.ppt']
 }, ['blocking']);
